f536d53d5c3382d3155bb6a1550b1dd79ed831a9
Maya Shaked <mayaromyshaked@yahoo.com>
2020-09-13 14:27:33 -0400

    prettify format

--- journal.py
+++ journal.py
@@ -1,20 +1,29 @@
-import pandas as pd 
-import numpy as np
 from datetime import date
-import matplotlib.pyplot as plt
-from wordcloud import WordCloud, STOPWORDS
 import os
+import string
+
+# Data cleaning and modeling
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-import string
+import pandas as pd
+import numpy as np
+
+# Visualization
+import matplotlib.pyplot as plt
+from matplotlib import colors
+from wordcloud import WordCloud, STOPWORDS
 
 DIRNAME = os.path.dirname(os.path.abspath("__file__"))
 PATH_TO_CSV = os.path.join(DIRNAME, 'daylio_export.csv')
-ORDERED_MOODS = ['awful', 'bad', 'meh', 'good', 'rad']
-SCALE = 10
+
+ORDERED_MOODS = ['awful', 'bad', 'meh', 'good',
+                 'rad']  # Daylio moods ordered from worst to best
+SCALE = 10  # The scale for your mood score. This means scores will be between 0 and range(SCALE)
 
 
-def gen_baseline_metrics(path_to_csv = PATH_TO_CSV, ordered_moods = ORDERED_MOODS, scale = SCALE):
+def gen_baseline_metrics(path_to_csv=PATH_TO_CSV,
+                         ordered_moods=ORDERED_MOODS,
+                         scale=SCALE):
 
     ugly_df = pd.read_csv(path_to_csv, index_col=False)
     df, all_activities = clean_df(ugly_df, ordered_moods, scale)
@@ -24,18 +33,27 @@ def gen_baseline_metrics(path_to_csv = PATH_TO_CSV, ordered_moods = ORDERED_MOOD
 
     linear_model = gen_linear_model(df, all_activities)
 
-    return(df)
+    return (df)
 
 
-def clean_df(df, ordered_moods = ORDERED_MOODS, scale = SCALE):
+def clean_df(df, ordered_moods=ORDERED_MOODS, scale=SCALE):
 
     df.full_date = pd.to_datetime(df.full_date)
-    df_with_one_hot_encoding, all_activities = convert_activities_to_categorical(df)
-    df_with_mood_scores = mood_to_score(df_with_one_hot_encoding, ordered_moods, scale)
-    
-    return(df_with_mood_scores, all_activities)
+    df_with_one_hot_encoding, all_activities = convert_activities_to_categorical(
+        df)
+    df_with_mood_scores = mood_to_score(df_with_one_hot_encoding,
+                                        ordered_moods, scale)
 
-def gen_dot_plot(df, ordered_moods = ORDERED_MOODS, matrix_length = 30):
+    return (df_with_mood_scores, all_activities)
+
+
+def gen_dot_plot(df, ordered_moods=ORDERED_MOODS, matrix_length=30):
+    '''
+    Generates a matrix dot plot visualization representing all my moods over time.
+
+    Input: Journal DataFrame, list of ordered moods, int of matrix length
+    Output: PyPlot figure
+    '''
 
     mood_categories = {}
     category = 0
@@ -49,17 +67,28 @@ def gen_dot_plot(df, ordered_moods = ORDERED_MOODS, matrix_length = 30):
 
     next_round_value = matrix_length * (len(mood_list) // matrix_length + 1)
     matrix_height = int(next_round_value / matrix_length)
-    mood_list_with_remainder = mood_list + [category] * (next_round_value - len(mood_list))
+    mood_list_with_remainder = mood_list + [category] * (next_round_value -
+                                                         len(mood_list))
 
     all_mood_array = []
 
     for x in range(matrix_height):
-        new_row = mood_list_with_remainder[(x * matrix_length):((x + 1) * matrix_length)]
+        new_row = mood_list_with_remainder[(x * matrix_length):((x + 1) *
+                                                                matrix_length)]
         all_mood_array.append(new_row)
 
     all_mood_array = np.array(all_mood_array)
 
-    return(all_mood_array)
+    plt.figure(figsize=(5, 5))
+    colormap = colors.ListedColormap([
+        "darkgreen", "forestgreen", "limegreen", "yellowgreen", "greenyellow",
+        "darkgrey"
+    ])
+    plt.imshow(data, cmap=colormap)
+    plt.axis('off')
+    plt.show()
+
+    return (plt)
 
 
 def gen_linear_model(df, all_activities):
@@ -69,16 +98,19 @@ def gen_linear_model(df, all_activities):
     df = df.select_dtypes(include='bool')
     X = df.astype(float).to_numpy()
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_sate=42) 
+    X_train, X_test, y_train, y_test = train_test_split(X,
+                                                        y,
+                                                        test_size=0.33,
+                                                        random_sate=42)
 
     reg = LinearRegression().fit(X_train, y_train)
     reg.sore(X, y)
     prediction = reg.predict(X_test)
 
-    return(prediction, Y_test)
+    return (prediction, Y_test)
 
 
-def mood_to_score(df, ordered_moods = ORDERED_MOODS, scale = SCALE):
+def mood_to_score(df, ordered_moods=ORDERED_MOODS, scale=SCALE):
     '''
     Input: 
         DataFrame of journal entries
@@ -100,12 +132,13 @@ def mood_to_score(df, ordered_moods = ORDERED_MOODS, scale = SCALE):
     ordered_mood_scores = {}
     for mood in original_metric.keys():
         value = original_metric[mood]
-        weighted_score = scale / (old_max - old_min) * (value - old_max) +  scale
+        weighted_score = scale / (old_max - old_min) * (value -
+                                                        old_max) + scale
         ordered_mood_scores[mood] = weighted_score
 
     df['mood_score'] = df['mood'].map(ordered_mood_scores)
 
-    return(df)
+    return (df)
 
 
 def convert_activities_to_categorical(df):
@@ -139,12 +172,23 @@ def convert_activities_to_categorical(df):
                     activity_list_binary.append(False)
         categorical_activity_matrix.append(activity_list_binary)
 
-
-    categorical_df = pd.DataFrame(categorical_activity_matrix, columns = all_activities)
+    categorical_df = pd.DataFrame(categorical_activity_matrix,
+                                  columns=all_activities)
 
     full_df = pd.concat([df, categorical_df], axis=1)
 
-    return(full_df, all_activities)
+    return (full_df, all_activities)
+
+
+def gen_hist(df, ordered_moods=ORDERED_MOODS):
+
+    moods = df[['mood']]
+
+    sns.set()
+    sns.countplot(moods['mood'], order=ORDERED_MOODS, color='gray')
+    plt.title("mood distribution")
+
+    return (plt)
 
 
 def gen_entries_over_time_hist(df):
@@ -159,46 +203,60 @@ def gen_entries_over_time_hist(df):
     end_year = latest_entry.year
     end_month = latest_entry.month
 
-    all_months = [date(m//12, m%12+1, 1) for m in range(start_year*12+start_month-1, end_year*12+end_month)]
+    all_months = [
+        date(m // 12, m % 12 + 1, 1)
+        for m in range(start_year * 12 + start_month - 1, end_year * 12 +
+                       end_month)
+    ]
     num_entries = []
 
     for month in all_months:
         num_entries.append(len(df[df._month == month]))
 
     ax = plt.subplot(111)
-    ax.bar(all_months, num_entries, width = 25, color = "darkorange")
+    ax.bar(all_months, num_entries, width=25, color="darkorange")
     ax.xaxis_date()
     plt.title("# journal entries written, by month")
 
-    return(plt)
+    return (plt)
 
-def gen_wordcloud(df, ordered_moods = ORDERED_MOODS):
+
+def gen_wordcloud(df, ordered_moods=ORDERED_MOODS):
 
     all_words = ''
     stopwords = set(STOPWORDS)
 
     words_by_mood = {}
+
     for mood in ordered_moods:
+
         words_by_mood[mood] = ''
 
     for index, row in df.iterrows():
+
         mood = row[['mood']].mood
         note = row[['note']].note
         if type(note) == str:
-            clean_note = note.translate(str.maketrans('', '', string.punctuation)).lower()
+            clean_note = note.translate(
+                str.maketrans('', '', string.punctuation)).lower()
         words_by_mood[mood] += clean_note
 
-    
     for mood, words in words_by_mood.items():
-        wordcloud = WordCloud(width = 800, height = 800, background_color ='white', stopwords = stopwords, min_font_size = 10, max_words = 100).generate(words)
-        plt.figure(figsize = (8,8), facecolor=None)
+        wordcloud = WordCloud(width=800,
+                              height=800,
+                              background_color='white',
+                              stopwords=stopwords,
+                              min_font_size=10,
+                              max_words=100).generate(words)
+        plt.figure(figsize=(8, 8), facecolor=None)
         plt.imshow(wordcloud)
         plt.axis("off")
-        plt.tight_layout(pad = 0)
+        plt.tight_layout(pad=0)
 
         plt.savefig(mood + '.png')
 
     pass
+
 
 if __name__ == '__main__':
     print(gen_baseline_metrics())
